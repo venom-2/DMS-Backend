ea966269c298fc5d1509fff9f4866ac4b4c4b917
Krishna <kdwivedi343@gmail.com>
2024-08-05 16:02:41 +0530

    Added fetch all Subjects route

--- routes/Fetch/Subject.js
+++ routes/Fetch/Subject.js
@@ -44,6 +44,39 @@ router.get('/subject/:id', async (req, res) => {
 });
 
 // Fecth Subject by year
+router.post('/subject', async (req, res) => {
+    try {
+        // Check if user is authorized
+        const authToken = req.header('authToken');
+        if (!authToken) {
+            return res.status(401).json({ message: 'Access denied!', success: false });
+        }
+
+        // Verify the token
+        let payload;
+        try {
+            payload = jwt.verify(authToken, process.env.JWT_SECRET);
+        } catch (error) {
+            return res.status(401).json({ message: 'Invalid token', success: false });
+        }
+
+        const { year } = req.body;
+
+        // Fetch Subject from DB
+        // Fetch all subjects
+        const subjects = await Subject.find({year}); 
+
+        // Return Subjects
+        res.status(200).json({ success: true, subjects });
+
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: 'Server Error', success: false });
+    }
+
+});
+
+// Fecth All Subject
 router.get('/subjects', async (req, res) => {
     try {
         // Check if user is authorized
