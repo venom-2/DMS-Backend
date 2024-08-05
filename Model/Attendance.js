1c6a7dbba90de2338a51e35ce2dfc720c8e2b3fe
Krishna <kdwivedi343@gmail.com>
2024-08-05 16:35:24 +0530

    Minor Fix

--- Model/Attendance.js
+++ Model/Attendance.js
@@ -5,7 +5,7 @@ const attendanceSchema = new mongoose.Schema({
     student: { type: mongoose.Schema.Types.ObjectId, ref: 'Student', required: true },
     subject: { type: mongoose.Schema.Types.ObjectId, ref: 'Subject', required: true },
     semester: { type: Number, required: true },
-    year: { type: Number, required: true },
+    year: { type: String, required: true },
     branch: { type: String, required: true },
     section: { type: String, required: true },
     date: { type: Date, required: true },
