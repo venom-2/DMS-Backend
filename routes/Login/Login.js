95a8f3e3f7c460c1dbe2a9d262803af24630d0c3
Akshat Trivedi <akshattrivedi32@gmail.com>
2025-03-30 21:03:15 +0530

    login route modified for admin users

--- routes/Login/Login.js
+++ routes/Login/Login.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { body, validationResult } = require('express-validator');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
+const pool = require('../../db');
 
 
 router.post('/login', [
@@ -19,72 +20,49 @@ router.post('/login', [
             return res.status(400).json({ errors: errors.array(), success: false });
         }
 
-        // destructuring email and password from req.body
+        // destructuring email,password and role from req.body
         const { email, password, role } = req.body;
+        var dept = '';
 
-        // Fetch User from DB
-        // const user = await User.findOne({ email });
-
-        // Check if user exists
-        // if (!user) {
-        //     return res.status(404).json({ message: 'User not found', sucess: false });
-        // }
-
-        // Check if password is correct
-        // const isMatch = await bcrypt.compare(password, user.password);
-        // if (!isMatch) {
-        //     return res.status(401).json({ message: 'Invalid credentials', success: false });
-        // }
-
-        // Check if role id correct
-        // if (role !== user.role) {
-        //     return res.status(401).json({ message: 'Invalid credentials', sucess: false });
-        // }
-
-        // const payload = {
-        //     user: {
-        //         id: user.id,
-        //         role: user.role,
-        //         department: user.department,
-        //         assignedSubjects: user.assignedSubjects,
-        //         email: user.email,
-        //         name: user.name
-        //     }
-        // };
-
-        // const authToken = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '2h' });
-
-        // Return User
-        // return res.status(200).json({ message: 'Login successful! ', authToken, success: true });
-
-        const userResult = await pool.query('SELECT * FROM users WHERE email = $1 and role = $2',[email,role]);
-
+        const userResult = await pool.query('SELECT * FROM users WHERE email = $1 and role = $2', [email, role]);
+        if (role === 'Admin') {
+            dept = 'Admin-IMS';
+        }
+        else {
+            const userDept = await pool.query('select d.department_name from departments d, users u where u.department_id = d.department_id and email = $1', [email]);
+            dept = userDept.rows[0].department_name;
+        }
         console.log(userResult.rows);
 
         // check if user exists or not
-        if(userResult.rows.length === 0){
-            return res.status(401).json({'message': 'Invalid email or password'});
+        if (userResult.rows.length === 0) {
+            return res.status(401).json({ 'message': 'Invalid email or password' });
         }
 
         const user = userResult.rows[0];
+        console.log(user);
+        console.log(dept);
 
         // comparing password from user and from DB
-        const isMatch = bcrypt.compare(password,user.password);
-        if(!isMatch){
-            res.status(401).json({'message':'Invalid email or password'});
+        const isMatch = bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            res.status(401).json({ 'message': 'Invalid email or password' });
         }
 
         // generate jwt token
         const token = jwt.sign(
-            { userId: user.id, email: user.email, position: user.role },
+            { userId: user.id, first_name: user.first_name, last_name: user.last_name, email: user.email, role: user.role, department: dept },
             process.env.JWT_SECRET,
             { expiresIn: '1h' }
         );
+        console.log(token);
 
-        res.json({message: 'Login successful! ', token, success: true});
+        res.json({ message: 'Login successful! ', token, success: true });
 
 
     } catch (error) {
+
+        console.log(error);
 
         // Return Error
         res.status(500).json({ message: 'Internal Server Error', success: false });
